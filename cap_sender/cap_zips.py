b182abbabcca29e709885de9877c1e037d98f3e0
jamie-r-davis <jamjam@umich.edu>
2018-09-26 16:17:07 -0400

    Adding docstrings

--- cap_sender/cap_zips.py
+++ cap_sender/cap_zips.py
@@ -7,39 +7,10 @@ from zipfile import ZipFile
 from cap_sender.cap_index import CAPIndex
 
 
-def process_zips(src_dir, output_dir):
-    outfiles = []
-    # iterate over files in src_dir
-    for f in os.listdir(src_dir):
-        if not f.endswith('.zip'):
-            continue
-        src_path = os.path.join(src_dir, f)
-        out_path = os.path.join(output_dir, f)
-        temp_dir = tempfile.TemporaryDirectory()
-        print(f"Found {src_path}...")
-        # extract zip contents to temp dir
-        with ZipFile(src_path) as zf:
-            zf.extractall(temp_dir.name)
-        # convert any xml into csv
-        for f in os.listdir(temp_dir.name):
-            if f.endswith('.xml'):
-                infile = os.path.join(temp_dir.name, f)
-                outfile = infile + '.txt'
-                c = CAPIndex(infile)
-                c.to_csv(outfile, delimiter='\t')
-                os.remove(infile)
-        # re-zip files to output_dir
-        print(f"Writing {out_path}...")
-        with ZipFile(out_path, 'w') as zf:
-            for f in os.listdir(temp_dir.name):
-                out_file = os.path.join(temp_dir.name, f)
-                zf.write(out_file, f)
-        print(f"Complete.")
-        outfiles.append(out_path)
-    return outfiles
-
-
 class ZipProcessor:
+    """
+    Base class for handling zip files.
+    """
     zip_pattern = ''
     pdf_pattern = ''
 
@@ -48,20 +19,32 @@ class ZipProcessor:
 
     @classmethod
     def match(cls, filename):
+        """
+        Class method which, when the given filename
+        matches cls.zip_pattern, will return the class.
+        """
         bn = os.path.basename(filename)
         if re.match(cls.zip_pattern, bn):
             return cls(filename)
 
     def transform(self):
+        """
+        Method which will be called to perform transformations
+        on the files belonging to this class.
+        """
         pass
 
 
 class TransferProcessor(ZipProcessor):
+    """
+    Class for handling CommonApp transfer zip files, which all need
+    an index file to be generated for DIP.
+    """
     pdf_fieldnames=[]
 
     def transform(self):
         """
-        Create a csv listing each pdf file and attributes
+        Create an `index.txt` tsv listing each pdf file and attributes
         parsed from the filename.
         """
         with ZipFile(self.fn, 'r') as zf:
@@ -78,12 +61,20 @@ class TransferProcessor(ZipProcessor):
 
 
 class TransferAppProcessor(TransferProcessor):
+    """
+    TransferProcessor class for handling
+    CommonApp Transfer Application zip files.
+    """
     zip_pattern = r'\d+_\d+_\d+_TR_Applications\.zip'
     pdf_pattern = r'(?P<filename>TR_(?P<commonapp_id>\d+)_(?P<last_name>.+?)_(?P<first_name>.+?)_.+)'
     pdf_fieldnames = ['filename', 'commonapp_id', 'last_name', 'first_name']
 
 
 class TransferEvalProcessor(TransferProcessor):
+    """
+    TransferProcessor class for handling
+    CommonApp Transfer Evaluation zip files.
+    """
     zip_pattern = r'\d+_\d+_\d+_TR_Evaluations\.zip'
     pdf_pattern = r'(?P<filename>TR_(?P<commonapp_id>\d+)_(?P<last_name>.+?)_(?P<first_name>.+?)_(?P<doc_id>\d+)_Evaluation_(?P<recommender>.+?)_.+)'
     pdf_fieldnames = ['filename', 'commonapp_id', 'last_name', 'first_name',
@@ -91,6 +82,10 @@ class TransferEvalProcessor(TransferProcessor):
 
 
 class TransferTranscriptProcessor(TransferProcessor):
+    """
+    TransferProcessor class for handling
+    CommonApp Transfer Transcript zip files.
+    """
     zip_pattern = r'\d+_\d+_\d+_TR_College_Transcript\.zip'
     pdf_pattern = r'(?P<filename>TR_(?P<commonapp_id>\d+)_(?P<last_name>.+?)_(?P<first_name>.+?)_(?P<doc_id>\d+)_(?P<doc_type>Transcript)_(?P<college_code>\d+)_(?P<college_name>.+?)_(?P<submit_dt>.+?)\.pdf)'
     pdf_fieldnames = ['filename', 'commonapp_id', 'last_name', 'first_name',
@@ -98,7 +93,10 @@ class TransferTranscriptProcessor(TransferProcessor):
 
 
 class FreshmanProcessor(ZipProcessor):
-
+    """
+    Class for handling CommonApp Freshman zip files, which all need
+    to have the xml index file transformed into a tsv DIP index.
+    """
     def transform(self):
         temp_dir = tempfile.TemporaryDirectory()
         with ZipFile(self.fn, 'a') as zf:
@@ -117,8 +115,16 @@ class FreshmanProcessor(ZipProcessor):
 
 
 class FreshmanAppProcessor(FreshmanProcessor):
+    """
+    FreshmanProcessor class for handling
+    CommonApp Freshman Application zip files.
+    """
     zip_pattern = r'ugaappl_.+\.zip'
 
 
 class FreshmanFormsProcessor(FreshmanProcessor):
+    """
+    FreshmanProcessor class for handling
+    CommonApp Freshman School Forms zip files.
+    """
     zip_pattern = r'ugaapplsform_.+\.zip'
